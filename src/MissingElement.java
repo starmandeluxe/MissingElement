aaa1fba21b74a629c7609fd072a9f1ac5b659e94
Alex K <starmandeluxe2001@gmail.com>
2013-10-09 18:57:59 -0700

    Added comments

--- src/MissingElement.java
+++ src/MissingElement.java
@@ -1,3 +1,9 @@
+/*
+ * Function that finds the missing element in an array
+ * 
+ * ex: {1, 2, 3, 5} <- missing element is 4
+ */
+
 public class MissingElement {
 	
 	public static void main(String[] args)
@@ -14,7 +20,7 @@ public class MissingElement {
 		float len = A.length+1;
 		//formula to get expected sum of all elements in the array
 		float expectedsum = len/2 * (len + 1);
-		System.out.println("expected: " + expectedsum);
+		//System.out.println("expected: " + expectedsum);
 		//calculate actual sum of all elements in the array
 		float actualsum = 0;
 		
@@ -22,7 +28,7 @@ public class MissingElement {
 		{
 			actualsum += A[i];
 		}
-		System.out.println("actual: " + actualsum);
+		//System.out.println("actual: " + actualsum);
 		//missing number will be the difference
 		return (int)(expectedsum - actualsum);
 	}
